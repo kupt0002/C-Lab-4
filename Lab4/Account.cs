081b65517ec8d560e9c9b244cb85249adf300934
kupt0002 <kupt00002@algonquinlive.com>
2020-02-27 15:14:38 -0500

    first commit Lab-4 files

---
+++ Lab4/Account.cs
@@ -0,0 +1,127 @@
+﻿
+/*****************************************************************/
+/**                                                             **/
+/**  Student name                : Andrei Kuptsov               **/
+/**  il Address                  : kupt0002@algonquinlive.com   **/
+/**  Student Number              : 040971315                    **/
+/**  Course Number               : 3002X                        **/
+/**  Lab Section Number          : 010                          **/
+/**  Professor Name              : Wei Gong                     **/
+/**  Assignment Name/Number/Date : Lab4                         **/
+/**  Optional Comments           : None                         **/
+/**                                                             **/
+/*****************************************************************/
+
+///**************************************START OF CODE*********************************************************///
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    public class Account // initializing Class Account
+    {
+        //initializing properties of the Class Account
+        private int accountNumber;
+        private string ownerName;
+        private double balance;
+        private double monthlyDepositAmount;
+
+        // initializing contant properties of the Class Account
+        private static double monthlyFee = 4.0;
+        private static double monthlyInterestRate = 0.0025;
+        public static double MinimumInitialBalance = 1000;
+        public static double MinimumMonthDeposit = 50;
+
+        // // Constructor to create instsance of the object newAccount with three attributes
+        public Account(string name, double initialAmount, double monthlyDeposit)
+        {
+            accountNumber = RandomNumber(90000, 99999); // setting attribute for property AccountNumber using method to generate random number method
+            ownerName = name; // setting attribute property OwnerName of the Object
+            balance = initialAmount; // setting attribute Balance of the Object
+            monthlyDepositAmount = monthlyDeposit; // setting attribute MonthlyDepositAmount of the Object
+            
+        }
+
+        public string OwnerName
+        {
+            get { return ownerName; }
+        }
+
+        public int AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        
+        public double MonthlyFee
+        {
+            get { return monthlyFee; }
+        }
+
+        public double MonthlyInterestRate
+        {
+            get { return monthlyInterestRate; }
+        }
+
+
+
+        private int RandomNumber(int min, int max) // Method to create random number
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        public double Deposit() // method to add  monthly deposit amount to update balance
+        {
+            if (monthlyDepositAmount >= MinimumMonthDeposit) // check if monthly deposit is more then minimum alowed deposit
+            {
+                balance += monthlyDepositAmount; // update balance
+                return balance = Math.Round(balance, 2); // rounding up to 2 digits after decimal point
+            }
+
+            else // otherwise program terminates
+            {
+                Console.WriteLine("Minimum deposit is {0}. Have a nice day!", MinimumMonthDeposit);
+                Console.ReadLine();
+                throw new Exception("Minimum deposit is not met");
+            }
+            
+            
+        }
+
+        public double Withdrawal() // method to update balance after withdrawal monthly fees
+        {
+            if (balance < MonthlyFee) // check if balance is more then monthly fees withdrawal program terminates
+
+            {
+                Console.WriteLine("Insufficient balance! Have a nice day!");
+                Console.ReadLine();
+                throw new Exception("Insufficient balance!");
+            }
+
+            else 
+            
+            {
+                return balance -= MonthlyFee; // update balance after withdrawal monthly fees
+            }
+            
+        }
+
+        public double DepositInterest() // method to update balance after monthly deposit
+        {
+            return balance += balance * MonthlyInterestRate;
+        }
+
+    }
+
+}
+
+///**************************************END OF CODE*********************************************************///
---
+++ Lab4/Bank.cs
@@ -0,0 +1,104 @@
+﻿/*****************************************************************/
+/**                                                             **/
+/**  Student name                : Andrei Kuptsov               **/
+/**  il Address                  : kupt0002@algonquinlive.com   **/
+/**  Student Number              : 040971315                    **/
+/**  Course Number               : 3002X                        **/
+/**  Lab Section Number          : 010                          **/
+/**  Professor Name              : Wei Gong                     **/
+/**  Assignment Name/Number/Date : Lab4                         **/
+/**  Optional Comments           : None                         **/
+/**                                                             **/
+/*****************************************************************/
+
+///**************************************START OF CODE*********************************************************///
+
+
+using System;
+using System.Collections.Generic;
+//using System.Text;
+
+namespace Lab4
+{
+    public class Bank
+    {
+        static void Main(string[] args)
+        {
+            bool validChoice = true; // setting bool to true for loop
+            List<Account> accountList = new List<Account>(); // initializing List of objects to use it for printing
+            Console.Write("Enter the number of months to deposit: ");
+            int numberOfMonth = int.Parse(Console.ReadLine());
+
+            do  // start of do While loop 
+
+            {
+                Console.Write("\nEnter customer name: ");
+                string name = Console.ReadLine();
+                if (name == "") // check if user enters empty string
+
+                {
+                    validChoice = false; // if yes, then we set bool to false 
+                    break;
+                }
+
+                else // if user enters not empty string
+
+                {
+                    Console.Write("Enter {0}'s Initial Deposit Amount (minimum $1000.00): ", name);
+                    double initialAmount = double.Parse(Console.ReadLine()); // stting variable for initial amount 
+                    
+                    if (initialAmount < Account.MinimumInitialBalance) // check if initial amount id less then minimum deposit requirements
+                    {
+                        Console.WriteLine("Initial Deposit Amount is $1000.00"); 
+                        Console.ReadLine();
+                        break; // if less, then program terminates
+                    }
+                    
+                    Console.Write("Enter {0}'s Monthly Deposit Amount (minimum $50.00): ", name); 
+                    double monthlyDeposit = double.Parse(Console.ReadLine()); // stting variable for monthly deposit 
+
+                    if (monthlyDeposit < Account.MinimumMonthDeposit) // check if monthly deposit less then minimum requirements
+                    {
+                        Console.WriteLine("Minimal monthly deposit Amount is $50.00"); 
+                        Console.ReadLine();
+                        break; // if less, then program terminates
+                    }
+
+                    // Creating instance of the object newAccount with three attributes
+                    Account newAccount = new Account(name, initialAmount, monthlyDeposit); 
+                    
+                    for (int Index=0; Index < numberOfMonth; Index++) // for loop to calculate account balance every months 
+                    {
+                        newAccount.Withdrawal(); // calling method to withdraw monthly bank fees
+                        newAccount.DepositInterest(); // calling method to add monthly interest to account
+                        newAccount.Deposit(); // calling method to create monthly deposit to account
+
+                    }
+                        accountList.Add(newAccount); // using method to add values of object properties to list 
+                       
+                }
+
+            }
+
+            while (validChoice); // end of do while loop
+
+        Console.Write("\n");
+            
+            foreach(Account ac in accountList)
+            {
+                Console.Write("After {0} month, {1}'s account (#{2}), has a balance of: ${3}\n", numberOfMonth, ac.OwnerName, ac.AccountNumber, ac.Balance);
+            }
+
+        //for (int index = 0; index < accountList.Count; index++)// for loop to print out values from list to display result of the calculations for each account
+        //    {
+        //        Console.Write("After {0} month, {1}'s account (#{2}), has a balance of: ${3}\n", numberOfMonth, accountList[index].OwnerName, accountList[index+1].AccountNumber, accountList[index+2].Balance);
+        //    }
+        
+            Console.Write("\nPress Enter to complete");
+            Console.ReadLine();
+
+        }
+    }
+}
+
+///**************************************END OF CODE*********************************************************///
